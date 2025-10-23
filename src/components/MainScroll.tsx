4dd49583e3a5b4da34fca539aff5fa8f4fe09882
Syed Armash Hussain <armashash@gmail.com>
2025-10-24 03:55:21 +0500

    style(App): update background color and add borders to grid_projects; refactor App component for cleaner return

--- src/components/MainScroll.tsx
+++ src/components/MainScroll.tsx
@@ -32,7 +32,7 @@ function MainScroll() {
       scrub: isLastRow ? 0.1 : 0.5,
       onUpdate: (self) => {
         const newSize = 20 + self.progress * 50;
-        const newHeight = 300 + self.progress * 400;
+        const newHeight = 300 + self.progress * 300;
 
         row.style.setProperty("--cell-size", newSize + "%");
         row.querySelectorAll(".holder-img").forEach((cell: any) => {
