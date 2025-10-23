f0c76f72f39e4514a35c1dda51078075d31457e6
Armash <ahussain@digitalauxilius.com>
2025-10-23 19:19:39 +0500

    first commit

--- vite.config.ts
+++ vite.config.ts
@@ -3,12 +3,16 @@ import react from '@vitejs/plugin-react-swc'
 
 export default defineConfig({
   plugins: [react()],
+  define: {
+    'process.env.NODE_ENV': JSON.stringify('production'),
+    'process.env': {}
+  },
   build: {
     lib: {
-      entry: 'src/main.tsx', 
+      entry: 'src/main.jsx',
       name: 'MyScrollComponent',
       fileName: (format) => `my-scroll-component.${format}.js`,
-      formats: ['iife'] 
+      formats: ['iife']
     },
     rollupOptions: {
       external: ['react', 'react-dom'],
